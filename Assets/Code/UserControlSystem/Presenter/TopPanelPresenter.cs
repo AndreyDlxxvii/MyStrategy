ca8cd7050cb4f6943da6f5c0a5d943ed6c2d0cee
Dlxxvii <a@a.ru>
2022-02-27 16:33:54 +0300

    Add menu

--- Assets/Code/UserControlSystem/Presenter/TopPanelPresenter.cs
+++ Assets/Code/UserControlSystem/Presenter/TopPanelPresenter.cs
@@ -2,21 +2,22 @@ using System;
 using TMPro;
 using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 public class TopPanelPresenter : MonoBehaviour
 {
     [SerializeField] private TMP_Text _inputField;
+    [SerializeField] private Button _menuBtn;
+    [SerializeField] private GameObject _menuGO;
 
-    [Inject]
-    private void Init(ITimeModel timeModel)
+    [Inject] private void Init(ITimeModel timeModel)
     {
         timeModel.GameTime.Subscribe(seconds =>
         {
             var t = TimeSpan.FromSeconds(seconds);
-            _inputField.text = string.Format("{0:D2}:{1:D2}",
-                t.Minutes,
-                t.Seconds);
+            _inputField.text = string.Format("{0:D2}:{1:D2}", t.Minutes, t.Seconds);
         });
+        _menuBtn.OnClickAsObservable().Subscribe(_ => _menuGO.SetActive(true));
     }
 }
